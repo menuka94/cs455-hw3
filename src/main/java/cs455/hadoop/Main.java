4fe92514455cb3a0b02d41e6fcdd6229d57c58da
Menuka Warushavithana <menuka.14@cse.mrt.ac.lk>
2020-03-23 23:03:24 -0600

    setup project structure

---
+++ src/main/java/cs455/hadoop/Main.java
@@ -0,0 +1,12 @@
+package cs455.hadoop;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+    private static final Logger log = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        log.info("Hello World!");
+    }
+}
