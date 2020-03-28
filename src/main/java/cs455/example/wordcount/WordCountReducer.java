f810360d12c6253b4dde0624277fd53b2e6e618f
Menuka Warushavithana <menuka.14@cse.mrt.ac.lk>
2020-03-28 03:41:51 -0600

    add WordCount example

---
+++ src/main/java/cs455/example/wordcount/WordCountReducer.java
@@ -0,0 +1,22 @@
+package cs455.example.wordcount;
+
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+/**
+ * Reducer: Input to the reducer is the output from the mapper. It receives word, list<count> pairs.
+ * Sums up individual counts per given word. Emits <word, total count> pairs.
+ */
+public class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        int count = 0;
+        // calculate the total count
+        for (IntWritable val : values) {
+            count += val.get();
+        }
+        context.write(key, new IntWritable(count));
+    }
+}
