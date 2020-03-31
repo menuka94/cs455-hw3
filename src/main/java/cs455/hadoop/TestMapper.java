a6c73f9f9b02050fdb6fe72308c5a84e3d8929ce
Menuka Warushavithana <menuka.14@cse.mrt.ac.lk>
2020-03-31 03:39:47 -0600

    try: print state name

--- src/main/java/cs455/hadoop/TestMapper.java
+++ src/main/java/cs455/hadoop/TestMapper.java
@@ -1,28 +1,20 @@
 package cs455.hadoop;
 
 import java.io.IOException;
-import java.util.StringTokenizer;
-import org.apache.hadoop.io.IntWritable;
+import cs455.hadoop.util.DataFields;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
-public class TestMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+public class TestMapper extends Mapper<LongWritable, Text, Text, Text> {
     private static final Logger log = LogManager.getLogger(TestMapper.class);
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        StringTokenizer itr = new StringTokenizer(value.toString());
-        int i = 0;
-        while (itr.hasMoreTokens()) {
-            String nextToken = itr.nextToken();
-            log.info(nextToken);
-            context.write(new Text(nextToken), new IntWritable(1));
-            if (i++ > 3) {
-                break;
-            }
-        }
+        log.info("key: " + key.get());
+        String[] strings = value.toString().split(",");
+        log.info("State: " + strings[DataFields.STATE_NAME]);
     }
 }
