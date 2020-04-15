53e4602c91150f7ab4d8ad67c81f335d8431e812
Menuka Warushavithana <menuka.warushavithana@gmail.com>
2020-04-14 23:49:28 -0600

    run Q6-mean-so2-levels

--- scripts/cluster/run-mean-so2-levels.sh
+++ scripts/cluster/run-mean-so2-levels.sh
@@ -5,4 +5,4 @@ gradle build
 $HADOOP_HOME/bin/hadoop jar build/libs/cs455-hw3-1.0-SNAPSHOT.jar \
   cs455.hadoop.q6_meanSO2Levels.MeanSO2LevelsJob \
   /data/gases \
-  /data/output/mean-so2-levels
+  /home/output/mean-so2-levels
